0b875f7b5fea2f6ac8aae0b4ade599e3da139db7
Antonio1394 <antoniorivm1394@gmail.com>
2016-12-09 23:09:10 -0600

    [update] add param-data

--- form_params.js
+++ form_params.js
@@ -6,7 +6,7 @@ http.createServer(function(req,res){
     if(req.url.indexOf("favicon.ico")>0){return;}
     fs.readFile("./index.html",function(err,html){
         var html_string=html.toString();
-        var arreglo_parametros=[];
+        var arreglo_parametros=[],parametros={};
         var variables=html_string.match(/[^\{\}]+(?=\})/g);
         var nombre="";
         if(req.url.indexOf("?")>0){
@@ -17,6 +17,9 @@ http.createServer(function(req,res){
 
         for(var i=arreglo_parametros.length-1;i>=0;i--){
             var parametro =arreglo_parametros[i];
+            var param_data=parametro.split("=");
+            parametros[param_data[0]=param_data[1]];
+            eval
         };
         
         for(var i=variables.length-1;i>=0;i--)
