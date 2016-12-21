814ad1911d2e1b37cdeb8d1ac439ff27ea5c49e6
Antonio1394 <antoniorivm1394@gmail.com>
2016-12-20 23:31:34 -0600

    [add] finish routing :bomb:

--- routing/app.js
+++ routing/app.js
@@ -3,9 +3,14 @@ var express=require("express");
 var app=express();
 
 app.set("view engine","jade");
-
+/// verbo Http=> get/post/put/patch/options/headers/delete
+//REST
 app.get("/",function(req,res){
     res.render("index");
+});
+
+app.get("/:nombre",function(req,res){
+    res.render("form",{nombre:req.params.nombre});
 });
 
 app.post("/",function(req,res){
