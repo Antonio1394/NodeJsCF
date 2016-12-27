ba7750b5db85168e998a1c1404e00044c33d0c78
Antonio1394 <antoniorivm1394@gmail.com>
2016-12-26 23:48:26 -0600

    [add] error

--- proyecto/app.js
+++ proyecto/app.js
@@ -34,7 +34,9 @@ app.post("/users",function(req,res){
     console.log(user.password_confirmation);
 
     user.save(function(err){
-        
+        if(err){
+            console.log(String(err));
+        }
         res.send("recibimos tus datos");
     });
     
