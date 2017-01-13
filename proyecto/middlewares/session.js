2cc4690c4fbc4d931d8bd331b248ac7bfff6ce5f
Antonio1394 <antoniorivm1394@gmail.com>
2017-01-12 22:54:06 -0600

    [update] user in view

--- proyecto/middlewares/session.js
+++ proyecto/middlewares/session.js
@@ -1,19 +1,18 @@
-var User=require("./models/user").User;
+var User=require("../models/user").User;
 
 module.exports=function(req,res,next){
     if(!req.session.user_id){
         res.redirect("/login")
     }else{
-        User.findbyId(req.session.user_id,function(err,user){
+        User.findById(req.session.user_id,function(err,user){
             if(err){
                 console.log(err);
                 res.redirect("/login")
             }else{
-                res.locals={user:user};
+                res.locals={ user:user };
                 next();
             }
         });
-        next();
 
     }
 }
