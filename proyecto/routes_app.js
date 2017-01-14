9459b917fbdf391fcf5c480ef8a3b4098c3a2faa
Antonio1394 <antoniorivm1394@gmail.com>
2017-01-13 22:53:19 -0600

    [add] route REST

--- proyecto/routes_app.js
+++ proyecto/routes_app.js
@@ -6,4 +6,17 @@ router.get("/",function(req,res){
     res.render("app/home")
 });
 
+/* REST */
+
+router.route("imagenes/:id")
+    .get(function(req,res){
+
+    })
+    .put(function(req,res){
+
+    })
+    .delete(function(req,res){
+
+    });
+
 module.exports=router;
