40217591d36be5f32914477c3061b79af3ba18d2
Antonio1394 <antoniorivm1394@gmail.com>
2017-02-16 22:20:41 -0600

    [add] middelaware find image :pig:

---
+++ proyecto/middlewares/find_image.js
@@ -0,0 +1,10 @@
+module.exports=function(req,res,next){
+    Imagen.findById(req.parms.id,function(err,imagen){
+        if(imagen!=null){
+            res.locals.imagen=imagen;
+            next();
+        }else{
+            res.redirect("/app");
+        }
+    })
+}
