3dea50feef02f308b87359bcb6c646163f6138b6
Antonio1394 <antoniorivm1394@gmail.com>
2017-02-22 22:56:18 -0600

    [update] add permission

--- proyecto/middlewares/image_permission.js
+++ proyecto/middlewares/image_permission.js
@@ -3,8 +3,14 @@ var Imagen=require("../models/imagenes");
 module.exports=function(image,req,res){
     //TRUE-TIene Permisos
     //Falso-Si no tienes Permisos
-
     if(req.method==="GET" && req.path.indexOf("edit")<0){
         return true;
     }
+
+    if(image.creator._id.toString()==res.locals.user._id){
+        //esta imagen yo la subi
+        return true
+    }
+
+    return false;
 }
