6e113d9f0dc714ad7b866512ce21c3429e045aa0
Antonio1394 <antoniorivm1394@gmail.com>
2017-02-24 14:59:58 -0600

    [update]

--- proyecto/middlewares/image_permission.js
+++ proyecto/middlewares/image_permission.js
@@ -2,12 +2,13 @@ var Imagen=require("../models/imagenes");
 
 module.exports=function(image,req,res){
 
-    if(typeof image.creator=="undefined") return false;
+    
     //TRUE-TIene Permisos
     //Falso-Si no tienes Permisos
     if(req.method==="GET" && req.path.indexOf("edit")<0){
         return true;
     }
+    if(typeof image.creator=="undefined") return false;
 
     if(image.creator._id.toString()==res.locals.user._id){
         //esta imagen yo la subi
